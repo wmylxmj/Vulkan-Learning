cc416b8aa5d953d8b6b4fe3337a7627d36ce0c46
wmylxmj <1397322329@qq.com>
2026-03-07 19:37:20 +0800

    新建项目

---
+++ VulkanLearning/WinMain.cpp
@@ -0,0 +1,11 @@
+#include "Windows.h"
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_  HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+	MessageBoxA(NULL, "Hello, Vulkan!", "Vulkan Learning", MB_OK);
+	return 0;
+}
