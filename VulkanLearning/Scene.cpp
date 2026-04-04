e17510eb0d568622e4053d500ab3e8f491d4b84e
wmylxmj <1397322329@qq.com>
2026-04-04 13:51:36 +0800

    更新模型

--- VulkanLearning/Scene.cpp
+++ VulkanLearning/Scene.cpp
@@ -8,6 +8,7 @@
 #include <cstdio>
 #include <string>
 #include <thread>
+#include "stb/stb_image.h"
 
 SceneNode* s_pSphereNode = nullptr;
 
@@ -17,23 +18,27 @@ glm::mat4 s_projectionMatrix;
 void InitScene(int inCanvasWidth, int inCanvasHeight)
 {
 	s_viewMatrix = glm::lookAt(
-		glm::vec3(2.0f, 2.0f, 2.0f),
+		glm::vec3(200.0f, 200.0f, 200.0f),
 		glm::vec3(0.0f, 0.0f, 0.0f),
 		glm::vec3(0.0f, 1.0f, 0.0f)
 	);
-	s_projectionMatrix = glm::perspective(glm::radians(60.0f), 1280.0f / 720.f, 0.1f, 100.0f);
+	s_projectionMatrix = glm::perspective(glm::radians(60.0f), 1280.0f / 720.f, 0.1f, 1000.0f);
 
 	StaticMesh::Initialize();
 	VkDevice vulkanDevice = GetVulkanDevice();
 
 	s_pSphereNode = new SceneNode();
 	s_pSphereNode->m_staticMesh = new StaticMesh();
-	s_pSphereNode->m_staticMesh->InitFromFile("Resource/UnitSphere.obj");
+	s_pSphereNode->m_staticMesh->InitFromFile("Resource/Models/Planet/Planet.obj");
 	s_pSphereNode->m_staticMesh->m_material.Init("Resource/test.vsb", "Resource/test.fsb");
 
 	int imageWidth = 0;
 	int imageHeight = 0;
-	void* pixelData = nullptr;
+	int imageChannels = 0;
+	void* pixelData = stbi_load(
+		"Resource/Models/Planet/Texture/Planet_Diffuse.png",
+		&imageWidth, &imageHeight, &imageChannels, 4
+	);
 
 	Texture* pTexture = new Texture[1];
 	pTexture->format = VK_FORMAT_R8G8B8A8_UNORM;
@@ -44,8 +49,14 @@ void InitScene(int inCanvasWidth, int inCanvasHeight)
 		imageHeight,
 		pTexture->format,
 		VK_IMAGE_USAGE_SAMPLED_BIT,
-		VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT
+		VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
 	);
+	VkDeviceSize imageSize = imageWidth * imageHeight * 4;
+	void* pMemoryData = nullptr;
+	vkMapMemory(vulkanDevice, pTexture->memory, 0, imageSize, 0, &pMemoryData);
+	memcpy(pMemoryData, pixelData, imageSize);
+	vkUnmapMemory(vulkanDevice, pTexture->memory);
+
 	pTexture->imageView = GenImageView2D(
 		pTexture->image,
 		pTexture->format,
