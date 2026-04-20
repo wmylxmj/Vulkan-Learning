727ba590e187290d9767cd365aa4ccfd0ff21cbb
wmylxmj <1397322329@qq.com>
2026-04-20 20:35:21 +0800

    更新计算管线

--- VulkanLearning/Scene.cpp
+++ VulkanLearning/Scene.cpp
@@ -83,6 +83,95 @@ void InitScene(int inCanvasWidth, int inCanvasHeight)
 	s_pFrameBuffer = new FrameBuffer();
 	s_pFrameBuffer->InitWithSize(1280, 720);
 
+	{
+		VkDescriptorSetLayout descriptorSetLayout;
+		VkDescriptorSetLayoutBinding descriptorSetLayoutBindings[2] = {};
+		descriptorSetLayoutBindings[0].binding = 0;
+		descriptorSetLayoutBindings[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+		descriptorSetLayoutBindings[0].descriptorCount = 1; // ubo -> descriptor <- texture
+		descriptorSetLayoutBindings[0].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+		descriptorSetLayoutBindings[0].pImmutableSamplers = nullptr; // for texture
+
+		descriptorSetLayoutBindings[1].binding = 1;
+		descriptorSetLayoutBindings[1].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+		descriptorSetLayoutBindings[1].descriptorCount = 1; // ubo -> descriptor <- texture
+		descriptorSetLayoutBindings[1].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+		descriptorSetLayoutBindings[1].pImmutableSamplers = nullptr; // for texture
+
+		VkDescriptorSetLayoutCreateInfo descriptorSetLayoutCreateInfo = {};
+		descriptorSetLayoutCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+		descriptorSetLayoutCreateInfo.bindingCount = _countof(descriptorSetLayoutBindings);
+		descriptorSetLayoutCreateInfo.pBindings = descriptorSetLayoutBindings;
+
+		vkCreateDescriptorSetLayout(vulkanDevice, &descriptorSetLayoutCreateInfo, nullptr, &descriptorSetLayout);
+
+		VkPipelineLayout pipelineLayout;
+		VkPipelineLayoutCreateInfo pipelineLayoutCreateInfo = {};
+		pipelineLayoutCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+		pipelineLayoutCreateInfo.pushConstantRangeCount = 0;
+		pipelineLayoutCreateInfo.pPushConstantRanges = nullptr;
+		pipelineLayoutCreateInfo.setLayoutCount = 1;
+		pipelineLayoutCreateInfo.pSetLayouts = &descriptorSetLayout;
+		vkCreatePipelineLayout(vulkanDevice, &pipelineLayoutCreateInfo, nullptr, &pipelineLayout);
+
+		VkPipelineShaderStageCreateInfo shaderStageCreateInfo = {};
+		shaderStageCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+		shaderStageCreateInfo.stage = VK_SHADER_STAGE_COMPUTE_BIT;
+		shaderStageCreateInfo.module = CompileShader("Resource/test.csb");
+		shaderStageCreateInfo.pName = "main";
+
+		VkComputePipelineCreateInfo computePipelineCreateInfo = {};
+		computePipelineCreateInfo.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
+		computePipelineCreateInfo.stage = shaderStageCreateInfo;
+		computePipelineCreateInfo.layout = pipelineLayout;
+
+		VkPipeline computePipeline;
+		vkCreateComputePipelines(vulkanDevice, nullptr, 1, &computePipelineCreateInfo, nullptr, &computePipeline);
+
+		VkDescriptorSet descriptorSet;
+		VkDescriptorPoolSize descriptorPoolSize[1] = {};
+		descriptorPoolSize[0].type = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE;
+		descriptorPoolSize[0].descriptorCount = 32; // descriptor -> ubo, texture, sampler
+
+		VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {};
+		descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+		descriptorPoolCreateInfo.maxSets = 1;
+		descriptorPoolCreateInfo.poolSizeCount = _countof(descriptorPoolSize);
+		descriptorPoolCreateInfo.pPoolSizes = descriptorPoolSize;
+
+		VkDescriptorPool descriptorPool;
+		vkCreateDescriptorPool(GetVulkanDevice(), &descriptorPoolCreateInfo, nullptr, &descriptorPool);
+
+		VkDescriptorSetAllocateInfo descriptorSetAllocateInfo = {};
+		descriptorSetAllocateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+		descriptorSetAllocateInfo.descriptorPool = descriptorPool;
+		descriptorSetAllocateInfo.descriptorSetCount = 1;
+		descriptorSetAllocateInfo.pSetLayouts = &descriptorSetLayout;
+		if (vkAllocateDescriptorSets(
+			GetVulkanDevice(),
+			&descriptorSetAllocateInfo,
+			&descriptorSet
+		) != VK_SUCCESS)
+		{
+			OutputDebugStringA("Failed to allocate descriptor sets!");
+		}
+
+		VkCommandBuffer commandBuffer = CreateCommandBuffer();
+		BeginCommandBuffer(commandBuffer, VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
+		vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, computePipeline);
+		vkCmdBindDescriptorSets(
+			commandBuffer,
+			VK_PIPELINE_BIND_POINT_COMPUTE,
+			pipelineLayout,
+			0,
+			1,
+			&descriptorSet,
+			0,
+			nullptr
+		);
+		vkEndCommandBuffer(commandBuffer);
+	}
+
 	s_pFullScreenQuadNode = new SceneNode();
 	s_pFullScreenQuadNode->m_staticMesh = new StaticMesh();
 	s_pFullScreenQuadNode->m_staticMesh->SetVertexCount(4);
@@ -106,7 +195,7 @@ void InitScene(int inCanvasWidth, int inCanvasHeight)
 		"Resource/fsq.fsb"
 	);
 	s_pFullScreenQuadNode->m_staticMesh->m_material.m_primitiveTopology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP;
-	s_pFullScreenQuadNode->m_staticMesh->m_material.SetTexture2D(2, 0, s_pFrameBuffer->m_colorRenderTarget->imageView, sampler);
+	s_pFullScreenQuadNode->m_staticMesh->m_material.SetTexture2D(2, 0, pDiffuseTexture->imageView, sampler);
 }
 
 void RenderOneFrame(float inFrameTimeInSeconds)
